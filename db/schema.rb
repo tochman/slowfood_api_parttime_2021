1762c580a868c917c87986275c6890cff418366f
shagufta <shagufta.batool8@gmail.com>
2022-02-16 12:24:41 +0100

    add category arrtibute to model product and into database

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_11_084621) do
+ActiveRecord::Schema.define(version: 2022_02_16_112234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
-    t.string "name"
+    t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "price"
+    t.text "category"
   end
 
 end
