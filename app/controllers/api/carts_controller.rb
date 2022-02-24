36314f0db730702a1b2488ceed6deba9756ccd85
Thomas Ochman <thomas@dragster.se>
2022-02-24 10:52:53 +0100

    cleans up some code adds record not found rescue in carts controller

--- app/controllers/api/carts_controller.rb
+++ app/controllers/api/carts_controller.rb
@@ -1,9 +1,16 @@
 class Api::CartsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :product_not_found
   def create
     product = Product.find(params[:cart][:product_id])
     cart = Cart.create
     cart.products << product
 
     render json: { message: "#{cart.products.last.name} has been added to your order" }, status: 201
+  end
+
+  private 
+
+  def product_not_found
+    render json: { message: "We couldn't find the product you tried to add."}
   end
 end
--- spec/requests/api/api_responds_with_a_collection_of_product_spec.rb
+++ spec/requests/api/api_responds_with_a_collection_of_product_spec.rb
@@ -1,39 +1,40 @@
-RSpec.describe 'GET /api/products' do
+RSpec.describe 'GET /api/products', type: :request do
+  subject { response }
+
   describe 'successfully' do
-    let!(:products) { 3.times { create(:product) } }
+    let!(:products) { create_list(:product, 3) }
     before do
       get '/api/products'
     end
 
-    it 'is expected to return a 200 response status' do
-      expect(response).to have_http_status 200
-    end
+    it { is_expected.to have_http_status 200 }
 
     it 'is expected to return all products' do
       expect(response_json['products'].count).to eq 3
     end
-     
+
     it 'is expected to return products names' do
-      expect(response_json['products'].first['name']).to eq 'chicken wings'
+      expect(response_json['products'].first['name'])
+        .to eq 'chicken wings'
     end
 
     it 'is expected to return a category' do
-      expect(response_json['products'].first['category']).to eq 'starter'
+      expect(response_json['products'].first['category'])
+        .to eq 'starter'
     end
-    
   end
+  
   describe 'unsuccessfully' do
     describe 'when there are no products in the database' do
       before do
         get '/api/products'
       end
 
-      it 'is expected to return a 422 response status' do
-        expect(response).to have_http_status 422
-      end
+      it { is_expected.to have_http_status 422 }
 
       it 'is expected to return a message indicating that there are no products' do
-        expect(response_json['message']).to eq 'There are no products in the database.'
+        expect(response_json['message'])
+          .to eq 'There are no products in the database.'
       end
     end
   end
